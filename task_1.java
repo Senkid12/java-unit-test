c15a8e64d648cc98fb0d83da84b391e200d8ba22
Sergey Krokhin <serega.krokha@yandex.ru>
2023-11-10 00:48:39 +0300

    My unit tests

---
+++ task_1.java
@@ -0,0 +1,6 @@
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+	assertEquals("Ожидался возраст больше 18", true, isAdult);
+}
